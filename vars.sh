f5d3ebbaf79fbc75bb92d0dc988fb8718836e580
Primiano Tucci <primiano@chromium.org>
2014-11-10 18:37:38 +0000

    Use self-hosted repo. code.google.com is struggling with my packs.

--- vars.sh
+++ vars.sh
@@ -7,4 +7,5 @@ AUTOMERGER_BRANCH="master"
 
 BLINK_REPO="https://chromium.googlesource.com/chromium/blink.git"
 CHROMIUM_REPO="https://chromium.googlesource.com/chromium/src.git"
-MERGED_REPO="https://chrome.blink.automerger@code.google.com/p/chromium-blink-merge/"
+#MERGED_REPO="https://chrome.blink.automerger@code.google.com/p/chromium-blink-merge/"
+MERGED_REPO="ssh://chromium-blink-merge@git.primianotucci.com/data/repository/chromium-blink-merge.git"
