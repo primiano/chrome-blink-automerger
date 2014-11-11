fb87c4c7075fcf2d8c522ef0d169cdc3fd865024
Primiano Tucci <primiano@chromium.org>
2014-11-11 08:56:48 +0000

    Logging costmetic fix

--- automerger_loop.sh
+++ automerger_loop.sh
@@ -9,28 +9,32 @@ touch automerger.log
 touch automerger-full.log
 chmod 644 automerger.log automerger-full.log
 
+getdate() {
+  echo "$(date --utc)  [$(TZ='America/Los_Angeles' date +'%H:%M %Z')]"
+}
+
 while true; do
   echo 
-  echo "Starting automerger cycle $(date)"
+  echo "Starting automerger cycle $(getdate)"
 
   # Self-update
+  pkill git
   git -C "$(dirname $0)" pull --ff-only
   git -C "$(dirname $0)" clean -df
-  pkill git
 
   cat automerger.log >> automerger-full.log
   tail -n 10000 automerger-full.log > automerger-full.log.tmp
   mv -f automerger-full.log.tmp automerger-full.log
-  date > automerger.log
+  getdate > automerger.log
   "$(dirname $0)/automerger_iteration.sh" &>> automerger.log
   exit_code=$?
-  /bin/echo -e "                             "\
-      "*** *** *** exit_code=${exit_code} $(date) *** *** ***\n\n" \
+  /bin/echo -e "\n --- --- exit_code=${exit_code} $(getdate) --- ---\n\n" \
       >> automerger.log
 
   if [ $exit_code != 0 ]; then
-    echo "Automerger error (exit_code: $exit_code)"
+    echo "Automerger error. Exit_code=${exit_code} $(getdate)"
+  else
+    echo "Automerger cycled successfully $(getdate)"
   fi
-  echo "Automerger cycle ended $(date)"
   sleep ${AUTOMERGER_CYCLE_TIME_SEC}
 done
