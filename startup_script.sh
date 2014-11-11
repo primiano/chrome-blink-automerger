5cf8ce7f2fbc859b75adf18af9739cb80a2e2132
Primiano Tucci <primiano@chromium.org>
2014-11-11 04:37:58 +0000

    Switch to nginx

--- startup_script.sh
+++ startup_script.sh
@@ -25,7 +25,8 @@ apt-get update
 apt-get upgrade -y
 
 # Install git from wheezy-backports, the default one is ancient (1.7).
-apt-get install -y -t wheezy-backports git git-core curl python-zdaemon less vim
+apt-get install -y -t wheezy-backports git git-core curl python-zdaemon less \
+                      vim nginx
 
 # Write the "automerger" command to /usr/local/bin.
 cat >/usr/local/bin/automerger <<"EOF"
@@ -35,7 +36,7 @@ if [ `whoami` == automerger ]; then
 else
   SUDO="sudo sudo -u automerger "
 fi
-$SUDO zdaemon -C /automerger/zdaemon.conf "$@"
+$SUDO zdaemon -C /automerger/.zdaemon.conf "$@"
 EOF
 chmod +x /usr/local/bin/automerger
 
@@ -43,6 +44,25 @@ chmod +x /usr/local/bin/automerger
 # replace it with a symlink to true since we know the vm is never on battery.
 dpkg-divert --local --rename --add /sbin/on_ac_power
 ln -sf /bin/true /sbin/on_ac_power
+
+dpkg-divert /etc/nginx/sites-enabled/default
+cat >/etc/nginx/sites-enabled/default <<"EOF"
+  server {
+    listen 80 default_server;
+    listen [::]:80 default_server ipv6only=on;
+    root /automerger;
+    server_name localhost;
+    location / {
+      try_files $uri $uri/ =404;
+      index automerger.log;
+      autoindex on;
+    }
+    types {
+      text/plain txt log;
+    }
+  }
+EOF
+service nginx restart
 
 ################################################################################
 # The remainder of the script is run as the automerger user using sudo.        #
@@ -83,18 +103,18 @@ git -C "${AUTOMERGER_BIN}" reset -q --hard origin/master
 git -C "${AUTOMERGER_BIN}" clean -qdf
 
 # Fetch and run the git authentication daemon.
-GCOMPUTE_TOOLS=/automerger/gcompute-tools
-[ -d "${GCOMPUTE_TOOLS}" ] || {
-  git clone https://gerrit.googlesource.com/gcompute-tools "${GCOMPUTE_TOOLS}"
-}
-git -C "${GCOMPUTE_TOOLS}" fetch -q origin
-git -C "${GCOMPUTE_TOOLS}" reset -q --hard origin/master
-git -C "${GCOMPUTE_TOOLS}" clean -qdf
-killall git-cookie-authdaemon &>/dev/null || true
-#### "${GCOMPUTE_TOOLS}/git-cookie-authdaemon"  # TODO check here
+# GCOMPUTE_TOOLS=/automerger/gcompute-tools
+# [ -d "${GCOMPUTE_TOOLS}" ] || {
+#   git clone https://gerrit.googlesource.com/gcompute-tools "${GCOMPUTE_TOOLS}"
+# }
+# git -C "${GCOMPUTE_TOOLS}" fetch -q origin
+# git -C "${GCOMPUTE_TOOLS}" reset -q --hard origin/master
+# git -C "${GCOMPUTE_TOOLS}" clean -qdf
+# killall git-cookie-authdaemon &>/dev/null || true
+# "${GCOMPUTE_TOOLS}/git-cookie-authdaemon"  # TODO check here
 
 # Create zdaemon config file for automerger service.
-cat >/automerger/zdaemon.conf <<"EOF"
+cat >/automerger/.zdaemon.conf <<"EOF"
 <runner>
   program /automerger/bin/automerger_loop.sh
   directory /automerger/
@@ -113,12 +133,8 @@ EOF
 # Start automerger service.
 touch /automerger/automerger.log
 chmod 644 /automerger/automerger.log
+echo > /automerger/automerger.log
 automerger start
-
-# Restart the WWW log server.
-sudo -u nobody pkill -U nobody python
-sudo -u nobody sh -c 'nohup /automerger/bin/www-server.py \
-                      /automerger/automerger.log < /dev/null >/dev/null 2>&1 &'
 
 SUDOEOF
 )" automerger
