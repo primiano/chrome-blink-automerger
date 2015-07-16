15422896deca19c61f78e5aad1d0c7e7e37c2c60
Primiano Tucci <primiano@chromium.org>
2015-07-16 10:22:20 +0100

    Update branch config for M45

--- history_rewrite_scripts/config.py
+++ history_rewrite_scripts/config.py
@@ -12,9 +12,9 @@ CHROMIUM_REPO_URL = 'https://chromium.googlesource.com/chromium/src.git'
 # 'ref/in/chromium' -> 'ref/in/blink'
 BRANCHES_TO_MERGE = [
     ('refs/heads/master', 'refs/heads/master'),
-    ('refs/branch-heads/2311', 'refs/branch-heads/chromium/2311'),
     ('refs/branch-heads/2357', 'refs/branch-heads/chromium/2357'),
     ('refs/branch-heads/2403', 'refs/branch-heads/chromium/2403'),
+    ('refs/branch-heads/2454', 'refs/branch-heads/chromium/2454'),
 ]
 
 MERGE_MSG = """Merge Chromium + Blink git repositories
